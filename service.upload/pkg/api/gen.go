bbaa17c4db37dd9dbaa82b4ddfd1f084804230b3
Darren Semusemu <darrensemusemu@gmail.com>
2022-08-13 17:54:35 +0200

    feat(service.upload): config openapi gen

---
+++ service.upload/pkg/api/gen.go
@@ -0,0 +1,5 @@
+package api
+
+//go:generate oapi-codegen -config ../../config/oapi-codgen_server.yaml ../../../api/openapi-spec/upload.yaml
+//go:generate oapi-codegen -config ../../config/oapi-codgen_spec.yaml ../../../api/openapi-spec/upload.yaml
+//go:generate oapi-codegen -config ../../config/oapi-codgen_types.yaml ../../../api/openapi-spec/upload.yaml
